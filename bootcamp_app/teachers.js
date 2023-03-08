6c90eb54bc4de47c499e8c8bbbe1e49db899bdcc
Koji Eguchi <koji.eguchi@hotmail.com>
2023-03-08 15:13:52 -0500

    Implemented a query for name_of_teacher_that_assisted

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,24 @@
+const {Pool} = require('pg');
+
+const pool = new Pool({
+  user: 'kojieguchi',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+GROUP BY teachers.name, cohorts.name 
+ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
